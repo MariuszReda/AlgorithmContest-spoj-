7431097bbf58f61de0ececce73c382bce660c970
MariuszReda <69969867+MariuszReda@users.noreply.github.com>
2021-01-19 14:33:00 +0100

    ASD_1_1

---
+++ ASD_1_1.cs
@@ -0,0 +1,29 @@
+/*
+ASD_1_1 - Maximum number
+The given sequence of non-negative integers terminating in zero is given, where zero is not an element of the string.
+
+Find the maximum number in a given string.
+
+INPUT
+There is a sequence of non-negative integers in the range [1; 100], terminated with zero. Zero is used to determine the end of the string, therefore it will occur exactly once in the input. Each element of the string is given on a separate line.
+
+OUTPUT
+The output should be the value of the maximum number in the given string.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{	
+	unsigned x;
+	int max= 0;
+	
+	do
+	{
+		cin >> x;
+		if (x > max)
+			max = x;
+	} while (x != 0);
+	cout << max;
+}
